76ccf5cb747e1eba9502d7b7ee40aca5bc68694c
Kareem James <kareemdjames@gmail.com>
2018-08-27 19:54:33 -0400

    fixed typos

--- simple-io-client.js
+++ simple-io-client.js
@@ -1,9 +1,12 @@
-const io = require(socket.io - client);
-
+const io = require("socket.io-client");
 const clientSocket = io("http://localhost:1337", {
   path: "/socket.io"
 });
-
+clientSocket.on("connect", () => {
+  for (let i = 1; i <= 5; i++) {
+    clientSocket.emit("time");
+  }
+});
 clientSocket.on("got time?", (time, counter) => {
   console.log(counter, time);
 });
